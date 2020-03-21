9f8552cf6f835d8a72059eaf5b3283c9e8d20f55
Leszek Wiesner <l.wiesner@adawards.pl>
2020-03-21 15:11:03 +0100

    Contracts builds fix + ropsten etherscan link

--- client/src/components/txLink.js
+++ client/src/components/txLink.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import ContractsManager from '../helpers/contracts';
 
 const StyledTxLink = styled.a`
     color: #ccddff;
@@ -9,11 +10,21 @@ const StyledTxLink = styled.a`
     }
 `;
 
+const ROPSTEN_CHAIN_ID = 3;
+
 export default class TxLink extends Component {
+    state = { isRopsten: false }
+    async componentWillMount() {
+        const cm = new ContractsManager();
+        const web3 = await cm.getWeb3();
+        const chainId = await web3.eth.getChainId();
+        if (chainId === ROPSTEN_CHAIN_ID) this.setState({ isRopsten: true }); 
+    }
     render() {
         const { tx } = this.props;
+        const { isRopsten } = this.state;
         return (
-            <StyledTxLink href={ `https://etherscan.io/tx/${ tx }` } target={ '_blank' }>
+            <StyledTxLink href={ `https://${ isRopsten ? 'ropsten.' : '' }etherscan.io/tx/${ tx }` } target={ '_blank' }>
                 { tx.slice(0, 8) + '...' }
             </StyledTxLink>
         )
