69235834c41029b66b05d104d9feb88e1b353bd2
Leszek Wiesner <l.wiesner@adawards.pl>
2020-03-26 10:08:27 +0100

    Prod - keep secret in cofing

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 
 module.exports = {
-    sessionSecret: 'somesecretkey',
+    sessionSecret: process.env.SESSION_SECRET,
     mongoURI: process.env.MONGODB_URI,
     web3Provider: new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws/v3/50111644f2784c749cad5cc35c4f366e"),
 }
